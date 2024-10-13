029767cf8f4d3e8c76358bba3d1ad540e4e38f1a
kmcdonie <111715780+kmcdonie@users.noreply.github.com>
2024-10-13 15:46:49 -0400

    Final commit

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,57 @@
 import java.util.Scanner;
-import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        String playerA;
-        String playerB;
-        String playerAgain;
-        Random gen = new Random();
+        String playAgain;
 
         do {
-            playerA = getPlayerMove("A", in);
+            String playerA = getPlayerMove(in, "A");
+            String playerB = getPlayerMove(in, "B");
 
-            playerB = getPlayerMove("B", in);
-
-            String result = determineWinner(playerA, playerB);
-            System.out.println(result);
+            displayResult(playerA, playerB);
 
             System.out.println("Do you want to play again? (Y/N): ");
-            playAgain = in.nextLine().trim();
+            playAgain = in.next();
         } while (playAgain.equalsIgnoreCase("Y"));
 
-        System.out.println("Thanks for playing!");
+        in.close();
+        }
+    public static String getPlayerMove(Scanner in, String player) {
+        String move;
+        while (true) {
+            System.out.println("Enter a move from player" + player + "(R, P, S or r, p, s): ");
+            move = in.next();
+            move = move.toUpperCase();
+            if (move.equals("R") || move.equals("P") || move.equals("S")) {
+                break;
+            } else {
+                System.out.println("You have entered an invalid value. Please select from the options (R, P, or S)");
+            }
+        }
+        return move;
+    }
+
+    public static void displayResult(String playerA, String playerB) {
+        if (playerA.equals(playerB)) {
+            System.out.println(playerA + " vs " + playerB + " It's a tie!");
+        } else if ((playerA.equals("R") && playerB.equals("S")) ||
+                (playerA.equals("P") && playerB.equals("R")) ||
+                (playerA.equals("S") && playerB.equals("P"))) {
+            System.out.println(getWinningPhrase(playerA, playerB) + " Player A wins!");
+        } else {
+            System.out.println(getWinningPhrase(playerB, playerA) + " Player B wins!");
+        }
+    }
+
+    public static String getWinningPhrase(String winner, String loser) {
+        if (winner.equals("R") && loser.equals("S")) {
+            return "Rock breaks Scissors,";
+        } else if (winner.equals("P") && loser.equals("R")) {
+            return "Paper covers Rock,";
+        } else if (winner.equals("S") && loser.equals("P")) {
+            return "Scissors cuts Paper,";
+        }
+        return "";
     }
 }
